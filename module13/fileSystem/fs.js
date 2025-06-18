1514c7be1d923643f1a89ad909d25e1dea2b18a2
Tanim-Ahmmed <tanimahmmed234@gmail.com>
2025-06-18 23:41:57 +0600

    file system in synchronous system

--- module13/fileSystem/fs.js
+++ module13/fileSystem/fs.js
@@ -3,6 +3,7 @@ console.log("task 1")
 const text = "learning file system ";
 fs.writeFileSync('./hello.txt', text);
 console.log("task 3")
+
 const data = fs.readFileSync('./hello.txt', { encoding: 'utf8' });
 console.log("task 4")
 console.log(data);
