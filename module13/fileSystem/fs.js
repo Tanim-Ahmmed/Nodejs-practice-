03df414f85f9e3998574d9d4da93f779e3644146
Tanim-Ahmmed <tanimahmmed234@gmail.com>
2025-06-19 10:30:27 +0600

    asynchronous file system in node js

--- module13/fileSystem/fs.js
+++ module13/fileSystem/fs.js
@@ -1,9 +1,27 @@
-const fs = require('fs');
-console.log("task 1")
-const text = "learning file system ";
-fs.writeFileSync('./hello.txt', text);
-console.log("task 3")
+// const fs = require('fs');
+// console.log("task 1")
+// const text = "learning file system ";
+// fs.writeFileSync('./hello.txt', text);
+// console.log("task 3")
 
-const data = fs.readFileSync('./hello.txt', { encoding: 'utf8' });
-console.log("task 4")
-console.log(data);
+// const data = fs.readFileSync('./hello.txt', { encoding: 'utf8' });
+// console.log("task 4")
+// console.log(data);
+
+
+
+//async
+
+const fs = require('fs');
+
+console.log("task 1");
+
+fs.readFile('./hello.txt', 'utf8', (err, data) => {
+  if (err) {
+    console.error('Error reading file:', err);
+    return;
+  }
+  console.log('File content:', data);
+});
+
+    console.log('task 3');
