38a7b45fb535a8a6e33766532c6264acaec1766b
Tanim-Ahmmed <tanimahmmed234@gmail.com>
2025-06-27 20:49:25 +0600

    basic problem solve with js

--- module13/fileSystem/fs.js
+++ module13/fileSystem/fs.js
@@ -12,16 +12,36 @@
 
 //async
 
-const fs = require('fs');
+// const fs = require('fs');
 
-console.log("task 1");
+// console.log("task 1");
 
-fs.readFile('./hello.txt', 'utf8', (err, data) => {
-  if (err) {
-    console.error('Error reading file:', err);
-    return;
-  }
-  console.log('File content:', data);
-});
+// fs.readFile('./hello.txt', 'utf8', (err, data) => {
+//   if (err) {
+//     console.error('Error reading file:', err);
+//     return;
+//   }
+//   console.log('File content:', data);
+// });
+ 
+// fs.writeFile('./hello.txt', 'learning file system 2', (err) => {
+//   if (err) {    
+//     console.error('Error writing file:', err);
+//     return;
+//     }   
+    
+// });
 
-    console.log('task 3');
+//     console.log('task 3');
+
+
+
+ const arr = [4,6,3,7,2];
+ 
+ let max = arr[0];
+ for(let i = 1; i < arr.length; i++ ){
+   if(arr[i] > max){
+       max = arr[i]
+   }
+ }
+ console.log(max)
