a063a0a7e3f4b813ef03f29bd9142dffeec9df20
Tanim-Ahmmed <tanimahmmed234@gmail.com>
2025-06-28 19:28:55 +0600

    file system updated

--- module13/fileSystem/fs.js
+++ module13/fileSystem/fs.js
@@ -36,12 +36,49 @@
 
 
 
- const arr = [4,6,3,7,2];
+//  const arr = [4,6,3,7,2];
  
- let max = arr[0];
- for(let i = 1; i < arr.length; i++ ){
-   if(arr[i] > max){
-       max = arr[i]
-   }
- }
- console.log(max)
+//  let max = arr[0];
+//  for(let i = 1; i < arr.length; i++ ){
+//    if(arr[i] > max){
+//        max = arr[i]
+//    }
+//  }
+//  console.log(max);
+
+
+
+
+
+//  let name = "Tanim Ahmmed"
+
+// console.log(name.split(" ").reverse().join(" "))
+
+
+
+
+// let arr = [2, 4 , 6 , 8]
+
+// let sum = arr[0];
+
+// for (let i = 1; i < arr.length ; i++ ){
+//     sum += arr[i];
+// }
+// console.log(sum)
+
+
+
+
+
+
+
+// const arr = [2, 4 ,3, 6 , 8]
+
+// let even = 0 ;
+
+// for (let i = 0; i < arr.length ; i++ ){
+//    if(arr[i] %2 == 0 ){
+//        even ++;
+//    }
+// }
+// console.log(even)
